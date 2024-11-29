20e59b80cb0bb58cddf180a0e17afed9543135e4
KurbonIsmailov92 <kurbon.ismailov@gmail.com>
2024-11-29 15:53:48 +0500

    laracast-day 21 - Login and Registration controllers

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -117,10 +117,12 @@
     <header class="bg-white shadow">
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 sm:flex sm:justify-between">
             <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{$heading}}</h1>
+            @auth
             <div class="flex space-x-4">
                 <x-button href="/jobs/create">Create Job</x-button>
                 <x-button href="/posts/create">Create Post</x-button>
             </div>
+            @endauth
         </div>
     </header>
     <main class="h-full bg-grey-900">
